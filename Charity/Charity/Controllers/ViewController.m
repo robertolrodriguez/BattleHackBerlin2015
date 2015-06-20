d540c8ab971b3eb0bfc4c3ddb5bcace2a7a987d7
Oktawian Chojnacki <oktawian.chojnacki@allegro.pl>
2015-06-20 16:46:46 +0200

    Cleanup.

--- Charity/Charity/Controllers/ViewController.m
+++ Charity/Charity/Controllers/ViewController.m
@@ -41,7 +41,7 @@
 }
 
 - (void)viewDidAppear:(BOOL)animated {
-    
+
     [self.bank authorize];
 
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
