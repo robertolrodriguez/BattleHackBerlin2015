a67a00e0e5c2156d28794fd2f4ac390178c28ee2
Michal Banasiak <michal.banasiak@allegro.pl>
2015-06-20 23:06:46 +0200

    breakiew

---
+++ Charity/Charity/BreakView.h
@@ -0,0 +1,15 @@
+//
+//  BreakView.h
+//  Charity
+//
+//  Created by Michal Banasiak on 20.06.2015.
+//  Copyright (c) 2015 BattleHack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BreakView : UIView
+
++ (instancetype)breakView;
+- (void)start;
+@end
--- Charity/Charity/Controllers/ViewController.m
+++ Charity/Charity/Controllers/ViewController.m
@@ -13,113 +13,123 @@
 #import "ChairController.h"
 #import "TimerViewController.h"
 #import "ChairModel.h"
+#import "BreakView.h"
 
-@interface ViewController () <BankDelegate , ChairControllerDelegate, ConnectionDelegate>
-@property (nonatomic, strong) Bank *bank;
-@property (nonatomic, weak) IBOutlet KAProgressLabel *progressLabelBadPositionTime;
-@property (nonatomic, weak) IBOutlet KAProgressLabel *progressLabelSedentaryTime;
-@property (nonatomic, weak) IBOutlet UIImageView *silhuetteImageView;
+@interface ViewController () <BankDelegate, ChairControllerDelegate,
+                              ConnectionDelegate>
+@property(nonatomic, strong) Bank *bank;
+@property(nonatomic, weak)
+    IBOutlet KAProgressLabel *progressLabelBadPositionTime;
+@property(nonatomic, weak) IBOutlet KAProgressLabel *progressLabelSedentaryTime;
+@property(nonatomic, weak) IBOutlet UIImageView *silhuetteImageView;
 
-@property (strong, nonatomic) IBOutlet TimerViewController*timerViewController;
-@property (nonatomic, strong) ChairModel *chairModel;
+@property(strong, nonatomic) IBOutlet TimerViewController *timerViewController;
+@property(nonatomic, strong) ChairModel *chairModel;
 
-@property (nonatomic, strong) ChairController *chairController;
-@property (weak, nonatomic) IBOutlet UILabel *balanceLabel;
-@property (weak, nonatomic) IBOutlet UILabel *timeOfWorkLabel;
-@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
+@property(nonatomic, strong) ChairController *chairController;
+@property(weak, nonatomic) IBOutlet UILabel *balanceLabel;
+@property(weak, nonatomic) IBOutlet UILabel *timeOfWorkLabel;
+@property(weak, nonatomic) IBOutlet UILabel *statusLabel;
 
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
+  [super viewDidLoad];
 
-    self.bank = [[Bank alloc] initWithViewController:self];
-    self.bank.delegate = self;
+  self.bank = [[Bank alloc] initWithViewController:self];
+  self.bank.delegate = self;
 
-    self.timerViewController = [[TimerViewController alloc] initWithLabel:self.timeOfWorkLabel];
-    self.chairController = [[ChairController alloc] initWithSedentaryLabel:self.progressLabelSedentaryTime
-                                                               slouchLabel:self.progressLabelBadPositionTime
-                                                            silhuetteImage:self.silhuetteImageView
-                                                       workTimerController:self.timerViewController
-                                                   acceptableSedentaryTime:60.0 * 30.0
-                                                      acceptableSlouchTime:30.0];
-    self.chairController.delegate = self;
+  self.timerViewController =
+      [[TimerViewController alloc] initWithLabel:self.timeOfWorkLabel];
+  self.chairController = [[ChairController alloc]
+       initWithSedentaryLabel:self.progressLabelSedentaryTime
+                  slouchLabel:self.progressLabelBadPositionTime
+               silhuetteImage:self.silhuetteImageView
+          workTimerController:self.timerViewController
+      acceptableSedentaryTime:5
+         acceptableSlouchTime:30.0];
+  self.chairController.delegate = self;
 
-    [self updateBalance:self.bank.balance];
+  [self updateBalance:self.bank.balance];
 
-    self.chairModel = [ChairModel new];
-    self.chairModel.delegate = self.chairController;
-    self.chairModel.connectionDelegate = self;
+  self.chairModel = [ChairModel new];
+  self.chairModel.delegate = self.chairController;
+  self.chairModel.connectionDelegate = self;
 }
 
 - (IBAction)sit:(id)sender {
-    self.chairController.sat = !self.chairController.sat;
+  self.chairController.sat = !self.chairController.sat;
 }
 
 - (IBAction)slouch:(id)sender {
-    self.chairController.slouched = !self.chairController.slouched;
+  self.chairController.slouched = !self.chairController.slouched;
 }
 
 - (void)updateBalance:(CGFloat)balance {
-    self.balanceLabel.text = [NSString stringWithFormat:@"Donated: %.2f$", balance];
+  self.balanceLabel.text =
+      [NSString stringWithFormat:@"Donated: %.2f$", balance];
 }
 
 - (void)slouchingTimeExceeded {
-    [self.bank charge];
+  [self.bank charge];
 }
 
 - (void)sedentaryTimeExceeded {
-
+    [self showBreakView];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
-    [self.bank authorize];
+  //[self.bank authorize];
+
+}
+
+- (void)showBreakView {
+    BreakView *v = [BreakView breakView];
+    [self.view addSubview:v];
+    v.frame = CGRectMake(0, 0, self.view.bounds.size.width - 0,
+                         self.view.bounds.size.height - 0);
 }
 
 - (void)balanceDidChange {
 
-
-    [self updateBalance:self.bank.balance];
+  [self updateBalance:self.bank.balance];
 }
 - (void)paypalDidAuthorize {
-
-
 }
 
 - (void)newConnectionState:(ConnectionState)state {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        
-        self.statusLabel.textColor = [UIColor orangeColor];
-        switch (state) {
-            case ConnectionStateConnected:
-                self.statusLabel.text = @"Connected!";
-                self.statusLabel.textColor = [UIColor greenColor];
+  dispatch_async(dispatch_get_main_queue(), ^{
 
-                break;
-            case ConnectionStateConnetcting:
-                self.statusLabel.text = @"Connecting...";
-                break;
-            case ConnectionStateFoundDevice:
-                self.statusLabel.text = @"Found device";
-                break;
-            case ConnectionStateError:
-                self.statusLabel.text = @"Error :-(";
-                self.statusLabel.textColor = [UIColor redColor];
+    self.statusLabel.textColor = [UIColor orangeColor];
+    switch (state) {
+    case ConnectionStateConnected:
+      self.statusLabel.text = @"Connected!";
+      self.statusLabel.textColor = [UIColor greenColor];
 
-                break;
-            case ConnectionStateSearchingForDevice:
-                self.statusLabel.text = @"Searching for device";
-                break;
-            case ConnectionStateStartedConnection:
-                self.statusLabel.text = @"Started connecting";
-                break;
-            default:
-                break;
-        }
-    });
+      break;
+    case ConnectionStateConnetcting:
+      self.statusLabel.text = @"Connecting...";
+      break;
+    case ConnectionStateFoundDevice:
+      self.statusLabel.text = @"Found device";
+      break;
+    case ConnectionStateError:
+      self.statusLabel.text = @"Error :-(";
+      self.statusLabel.textColor = [UIColor redColor];
+
+      break;
+    case ConnectionStateSearchingForDevice:
+      self.statusLabel.text = @"Searching for device";
+      break;
+    case ConnectionStateStartedConnection:
+      self.statusLabel.text = @"Started connecting";
+      break;
+    default:
+      break;
+    }
+  });
 }
-
 
 @end
