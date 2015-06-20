45df951ce28f15c0ec390dd52933069dbe7f7b08
Kamil Pyć <kamil@jouret-mobile.be>
2015-06-20 14:19:15 +0200

    Added project

---
+++ Charity/Charity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Charity
+//
+//  Created by Kamil Pyć on 6/20/15.
+//  Copyright (c) 2015 BattleHack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Charity/Charity/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Charity
+//
+//  Created by Kamil Pyć on 6/20/15.
+//  Copyright (c) 2015 BattleHack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
