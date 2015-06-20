4587b0df87fec916b0eab85fb3523d4c4401bcaa
Michal Banasiak <michal.banasiak@allegro.pl>
2015-06-20 15:44:46 +0200

    balance

---
+++ Charity/Charity/BalanceView.h
@@ -0,0 +1,13 @@
+//
+//  BalanceView.h
+//  Charity
+//
+//  Created by Michal Banasiak on 20.06.2015.
+//  Copyright (c) 2015 BattleHack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BalanceView : UIView
+
+@end
---
+++ Charity/Charity/BalanceView.m
@@ -0,0 +1,21 @@
+//
+//  BalanceView.m
+//  Charity
+//
+//  Created by Michal Banasiak on 20.06.2015.
+//  Copyright (c) 2015 BattleHack. All rights reserved.
+//
+
+#import "BalanceView.h"
+
+@implementation BalanceView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
